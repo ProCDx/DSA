c653f655b8c0d5d52967885a1601cc021da4c54c
ProCDx <projayghosh6969@gmail.com>
2025-08-25 22:35:56 +0530

    Fixed frequency function bug in F1.cpp

---
+++ C++/Arrays/Frequency/F1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+
+int frequency(vector<int>& arr){
+    unordered_map<int,int> freq;
+
+    for(int num:arr){
+        freq[num]++;
+    }
+
+    int maxfreq=0,ans=-1;
+    for(auto& it:freq){
+        if(it.second>maxfreq){
+            maxfreq=it.second;
+            ans=it.first;
+        }
+    }
+    return ans;
+}
+int main(){
+    vector<int> arr={1, 3, 2, 3, 4, 3, 5, 2, 2, 2};
+    cout<<"The most frequent element: "<<frequency(arr)<<endl;
+    return 0;
+}
