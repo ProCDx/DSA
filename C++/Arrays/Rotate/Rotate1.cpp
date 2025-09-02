2fbf9fcb05a5bfab892b3e116c3f86a66f426bcc
ProCDx <projayghosh6969@gmail.com>
2025-09-02 14:08:57 +0530

    Updating

--- C++/Arrays/Rotate/Rotate1.cpp
+++ C++/Arrays/Rotate/Rotate1.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-void rotate(vector<int>& arr,int d){
+void rotate(vector<int> &arr,int d){
     int n=arr.size();
-
+    d=d%n;
     for(int i=0;i<d;i++){
         int last=arr[n-1];
         for(int j=n-1;j>0;j--){
@@ -14,9 +14,9 @@ void rotate(vector<int>& arr,int d){
 }
 int main(){
     vector<int> arr={ 1, 2, 3, 4, 5, 6 };
-    int d=2;
+    int d=4;
     rotate(arr,d);
-    for(int i=0;i<arr.size();i++){
+    for(int i=0;(int)i<arr.size();i++){
         cout<<arr[i]<<" ";
     }
     return 0;
